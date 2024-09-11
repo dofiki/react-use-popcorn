ab9befaf58894f4ebc9d9c1cedeb97ff97af4a79
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-09-11 21:08:35 +0545

    props as api
    done

--- src/components/stars.js
+++ src/components/stars.js
@@ -11,22 +11,16 @@ const starContainerStyle = {
     alignItems:"center",
 }
 
-const textStyle = {
-    fontWeight: "bold"
-}
+export default function StarRating({maxStars=5, color='black', size=30, message=[], defaultRating=0 }){ //setting default number of stars 
 
-const starStyle = {
-    display: "block",
-    height:"30px",
-    width:"30px",
-    cursor:"pointer"
-}
-
-
-export default function StarRating({maxStars=5}){ //setting default number of stars 
-
-    const [rating, setRating] = useState(0)
+    const [rating, setRating] = useState(defaultRating)
     const [tempRating, setTempRating] = useState(0)
+
+    const textStyle = {
+        fontWeight: "bold",
+        color:color,
+        fontSize:`${size}px`
+    }
 
     function handleRating(id){
         setRating(rating===id?0:id)
@@ -36,19 +30,33 @@ export default function StarRating({maxStars=5}){ //setting default number of st
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({length:maxStars}, (_,i)=>
-                <Star key={i} onRate={handleRating} id={i+1} 
-                      rating={rating}
+                <Star key={i} 
+                      rating={rating} 
+                      onRate={handleRating} id={i+1} 
                       tempRating={tempRating}
                       onHoverIn={()=>setTempRating(i+1)}
-                      onHoverOut={()=>setTempRating(0)}/>)}
+                      onHoverOut={()=>setTempRating(0)}
+                      color={color}
+                      size={size}/>)}
             </div>
-            <div style={textStyle}>{tempRating||rating||''}</div>
+            <div style={textStyle}>
+                {message.length===maxStars?message[tempRating?tempRating-1:rating-1]:tempRating||rating||''}
+                </div>
         </div>
     )
 }
 
-function Star({onRate,id,onHoverIn,onHoverOut,rating,tempRating}){
+function Star({onRate,id,onHoverIn,onHoverOut,rating,tempRating,color,size}){
     const logic = tempRating?tempRating:rating;
+
+    const starStyle = {
+        display: "block",
+        color:color,
+        height:`${size*1.5}px`,
+        width:`${size*1.5}px`,
+        cursor:"pointer"
+    }
+
     return (
         <span role="button" style={starStyle} onClick={()=>onRate(id)} 
         onMouseEnter={onHoverIn} onMouseLeave={onHoverOut}>
@@ -59,7 +67,7 @@ function Star({onRate,id,onHoverIn,onHoverOut,rating,tempRating}){
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="#000">
+                stroke={color}>
                     <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -69,8 +77,8 @@ function Star({onRate,id,onHoverIn,onHoverOut,rating,tempRating}){
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
-                fill="#000"
-                stroke="#000">
+                fill={color}
+                stroke={color}>
                     <path
                     d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"/>
             </svg>}
--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import Stars from './components/stars';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Stars maxStars={5} />
+    <Stars maxStars={5} size={40}  color="green"/>
+    <Stars maxStars={10} size={20} color="red" defaultRating={5} />
+    <Stars maxStars={5} size={20} color="black" 
+           message={["meh","ok","mid","good","hellyeah"]}
+           className="test"/>
   </React.StrictMode>
 );
 
